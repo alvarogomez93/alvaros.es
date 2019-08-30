3c21ce85469cca05baa935214b4600e2a2c35e20
algoprado93 <alvaro.gomez.93@outlook.es>
2019-08-30 09:04:24 +0000

    Fixing errors

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
+def homepage(path):
+    return render_template("main.html")
+    
+
 @app.route('/<path:path>')
 def catch_all(path):
     return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
