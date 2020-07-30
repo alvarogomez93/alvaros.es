af53405e757cc3decf597b0f5a6f37a4af09be89
alvarogomez93 <alvaro.gomez.93@outlook.es>
2020-07-30 15:54:16 +0200

    refactoring and new page

--- index.py
+++ index.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 def homepage(path):
     return render_template("main.html",title='Inicio')
-    
+
+@app.route('/calculator',defaults={'path': ''})
+def cafcalculator(path):
+    return render_template("calculator.html",title='Caffeine calculator')
+    #return render_template("calculator.html",title='Caffeine consumption calculator')
 
 @app.route('/<path:path>')
 def catch_all(path):
